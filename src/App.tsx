edef5bf8feb64626b99c2aaf78153fab27fc3421
Natalia <natalia.pmd@gmail.com>
2024-07-21 20:25:40 +0200

    Product List

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import MenuItem from "./components/MenuItem"
 import { menuItems } from "./data/db"
 
 function App() {
@@ -8,7 +9,23 @@ function App() {
         <h1 className="text-center text-2xl font-bold text-gray-500">Calculadora de propinas y consumo</h1>
       </header>
 
-
+    <main className="max-w-7xl mx-auto py-20 grid md:grid-cols-2">
+    <div className="p-5">
+        <h2 className="text-3xl font-black">Menu</h2>
+        <div className="space-y-3 mt-10">
+          {menuItems.map(item => (
+            <MenuItem
+              key={item.id}
+              item={item}
+            />
+          ))}
+        </div>
+      </div>
+      <div>
+        <h2 className="text-3xl font-black">Consumo</h2>
+      </div>
+      
+    </main>
 
     </>
   )
---
+++ src/components/MenuItem.tsx
@@ -0,0 +1,16 @@
+import { MenuItem } from "../types"
+
+type MenuItemProps = {
+    item: MenuItem
+}
+
+export default function MenuItem({item} : MenuItemProps) {
+    return (
+        <button
+            className="border-2 border-teal-100 hover:bg-teal-100 w-full p-3 flex justify-between"
+        >
+            <p>{item.name}</p>
+            <p className="font-black">${item.price}</p>
+        </button>
+    )
+}

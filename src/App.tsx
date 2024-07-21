bde91fc3ca95782e618ddb1d6b3b0186f89116ae
Natalia <natalia.pmd@gmail.com>
2024-07-21 19:34:52 +0200

    Init project

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { menuItems } from "./data/db"
+
+function App() {
+
+  return (
+    <>
+      <header className="bg-teal-100 py-5">
+        <h1 className="text-center text-2xl font-bold text-gray-500">Calculadora de propinas y consumo</h1>
+      </header>
+
+
+
+    </>
+  )
+}
+
+export default App

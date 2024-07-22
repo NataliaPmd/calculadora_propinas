d9db6062428c4f101167df83001fc5bc6d762ccb
Natalia <natalia.pmd@gmail.com>
2024-07-22 19:30:49 +0200

    Add item to order

--- src/components/MenuItem.tsx
+++ src/components/MenuItem.tsx
@@ -2,12 +2,14 @@ import { MenuItem } from "../types"
 
 type MenuItemProps = {
     item: MenuItem
+    addItem: (item: MenuItem) => void
 }
 
-export default function MenuItem({item} : MenuItemProps) {
+export default function MenuItem({item, addItem} : MenuItemProps) {
     return (
         <button
-            className="border-2 border-teal-100 hover:bg-teal-100 w-full p-3 flex justify-between"
+            className="border-2 border-teal-100 hover:bg-teal-100 w-full p-3 text-lg rounded-lg flex justify-between"
+            onClick={()=> addItem(item)}
         >
             <p>{item.name}</p>
             <p className="font-black">${item.price}</p>
---
+++ src/hooks/useOrder.ts
@@ -0,0 +1,25 @@
+import { useState } from "react"
+import type { MenuItem, OrderItem } from "../types"
+
+export default function useOrder() {
+    const [order, setOrder] = useState<OrderItem[]>([])
+
+
+    const addItem = (item: MenuItem) => {
+        const itemExist: number = order.findIndex(order => item.id === order.id)
+        if(itemExist >= 0) {
+            const updatedOrder: OrderItem[] = [...order]
+            updatedOrder[itemExist].quantity++
+            setOrder(updatedOrder)
+        } else {
+            const newItem: OrderItem = {...item, quantity:1}
+            setOrder([...order, newItem])
+        }
+    }
+
+
+
+    return {
+        addItem
+    }
+}
